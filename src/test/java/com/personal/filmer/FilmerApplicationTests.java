bbb4a95451d362ca1e22cc8cfd70fa615060315f
Jedidiah <jedidiah.hwang.dev@gmail.com>
2023-05-15 11:11:59 -0600

    initial project created

---
+++ src/test/java/com/personal/filmer/FilmerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.filmer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
